cd7b6f752d30d31929185bcaf1a1059ef9c0878f
Kerpackie <ckeleo@pm.me>
2024-11-03 09:51:44 +0000

    Insert Image Resize

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -19,44 +19,85 @@ class Home extends BaseController {
 			. view ('templates/footer');
 	}
 
-	public function insert() {
+	public function insert(){
+		
 		$data = []; $msg = "";
+		
 		//load CI form helper
 		helper(['form']);
+		
 		//if the user has submitted the form
 		if ($this->request->getMethod() == 'POST') {
-			
+
 			//set up the validation rules
-			$rules = [	'firstName'  => 'required',
-               			'lastName' => 'required|min_length[2]',
-               			'yearBorn'   => 'required|numeric'  ];
+            $rules = [ 'firstName'  => 'required',
+						'lastName'  => 'required|min_length[2]',
+						'yearBorn'  => 'required|numeric'	];
+			
 			if(!$this->validate($rules))
-				//form fields have not passed validation - prepare errors for display			
+				//form fields have not passed validation - prepare errors for display
 				$data['validation'] = $this->validator;
 			else {
 				//form fields have passed validation
-				//load the author model
-				$model = new Author_Model;
-				//get values from post
-				$anAuthor = [	'firstName' => $_POST['firstName'],
-								'lastName'  => $_POST['lastName'],
-								'yearBorn'  => $_POST['yearBorn']  ];
-				//check if insert to database is successful – display appropriate message
-				if ($model->save($anAuthor)) {
-					$msg .= "<br><br>The insert to database has been successful<br><br>";				}
-				else {
-					$msg .= "<br><br>Uh oh ... problem on insert to database<br><br>";
-				}
-				//load the view to display the message
+
+				//validate the image - correct file type & within max size
+				$validateImg = $this->validate([
+					'file' => [ 'uploaded[file]',
+                    'mime_in[file,image/jpg,image/jpeg,image/png,image/gif]',
+                    'max_size[file,4096]', ]  ]);
+
+                //if image not valid output message else do upload & resize & database insert
+				if(!$validateImg) {
+                	$msg .= "<br><br>Either file type or size (Max 4MB) not correct to create image.";
+					$msg .= "<br><br>Insert not done.";
+                } else {
+					//upload file
+                    $x_file = $this->request->getFile('file');
+                    //the upload process may move the file to an intermediate location 
+					//& change the name, this ensures we use the original file name
+					$originalName = $x_file->getClientName();
+
+					//resize to our required full format and put in full folder
+					$image = \Config\Services::image()
+							->withFile($x_file)
+                        	->resize(345, 186, true, 'height')
+                        	->save(FCPATH.'/assets/images/full/'.$originalName);
+					$msg .= "<br><br>Upload done & image resized<br><br>";
+						
+                    //resize to thumb format and put in thumbs folder
+					$image = \Config\Services::image()
+                     		->withFile($x_file)
+                        	->resize(140, 76, true, 'height')
+                        	->save(FCPATH.'/assets/images/thumbs/'.$originalName);
+					$msg .= "<br><br>image resized to thumbnail<br><br>";  
+				
+					//load the author model
+					$model = new Author_Model;
+		
+					//get values from post 
+					//include the image name
+					$anAuthor = [ 	'firstName' => $_POST['firstName'],
+									'lastName'  => $_POST['lastName'],
+									'yearBorn'  => $_POST['yearBorn'],
+									'image'       => $originalName  ];
+
+					//check if insert to database is successful – display appropriate message
+					if ($model->save($anAuthor)) {
+						$msg .= "<br><br>The insert to database has been successful<br><br>";
+					} else {
+						$msg .= "<br><br>Uh oh ... problem on insert to database<br><br>";
+					}
+                }				
+				//load the view to display the error / information message
 				$data['message'] = $msg;
-                return view('templates/header')
+				return view('templates/header')
 					. view('displayMessageView', $data)
 					. view('templates/footer');
-			} // end insert to database
-		} // end if POST
+			}
+		}
 		//load the insert author view
-        	return view('templates/header', $data)
-				. view('insertAuthorView')
-				. view('templates/footer');
+        		return view('templates/header', $data)
+			. view('insertAuthorView')
+			. view('templates/footer');
 	} //end insert
 } //end class
--- app/Views/insertAuthorView.php
+++ app/Views/insertAuthorView.php
@@ -1,14 +1,21 @@
-<br><br><br>
-<form action="<?php echo base_url();?>insert" method="post"> 
-	First Name:
-		<input  type="text"   name="firstName" id="firstName" value="<?php echo set_value('firstName')?>"> </br></br>
-	Last Name: 
-		<input  type="text"   name="lastName"  id="lastName"  value="<?php echo set_value('lastName')?>"> </br></br> 	
-	Year Born:
-		<input  type="text"   name="yearBorn"  id="yearBorn"  value="<?php echo set_value('yearBorn')?>"> </br></br>
-	<button type="submit" name="Insert"    id="Insert">Insert</button>
+	<br><br><br>
+	<!-- open form as multipart because it will contain a file upload -->
+	<form action="<?php echo base_url();?>insert" method="post" enctype="multipart/form-data">
+
+		First Name:
+			<input  type="text"   name="firstName" id="firstName" value="<?php echo set_value('firstName')?>"> </br></br>
+		Last Name: 
+			<input  type="text"   name="lastName"  id="lastName"  value="<?php echo set_value('lastName')?>"> </br></br> 	
+		Year Born:
+			<input  type="text"   name="yearBorn"  id="yearBorn"  value="<?php echo set_value('yearBorn')?>"> </br></br>
+
+		</br></br>
+        Image Upload:
+        	<input type="file" id="file" name="file" value="<?= set_value('userFile') ?>"> </br></br>
+
+		<button type="submit" name="Insert"    id="Insert">Insert</button>
 	
-	<?php if (isset($validation))
-		echo $validation->listErrors() ?>
+		<?php if (isset($validation))
+			echo $validation->listErrors() ?>
 	
-</form>
+	</form>
